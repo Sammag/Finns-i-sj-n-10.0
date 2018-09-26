05009f51154e1a374b6be60c7f70fccd3a752f0c
Samuel Magnusson <masa16@nti.umea>
2018-09-26 13:48:13 +0200

    jag har skrivit början av finns i sjön programet

---
+++ finns i sjön.py
@@ -0,0 +1,21 @@
+import random
+ai = random.randint (1,4)
+array([[4, 0, 2, 1],
+       [3, 2, 2, 0]])
+meny = 0
+
+print("Hej och välkommen till mitt finns i sjön")
+print("välj 1-4 för lycka eller cancer")
+print("väjl val 1: ♠")
+print("välj val 2: ♥")
+print("välj val 3: ♦")
+print("välj val 4: ♣")
+
+elif val ==1:
+    
+elif val ==2: 
+       
+elif val ==3:
+        saldo= int(input("din saldo "))
+elif val ==4:
+input("tack för att du använde bankdank")
